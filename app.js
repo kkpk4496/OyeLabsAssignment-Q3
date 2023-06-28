97410a2c0599d08f440d354327ac28f2c562f1f6
kkpk4496 <pavankalyank4496@gmail.com>
2023-06-28 07:42:19 +0000

    message

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
-const person = new Object();
-person.id = 2;
-person.gender = "male";
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
 
-const student = new Object();
-student.name = "ravi";
-student.email = "ravi11@yopmail.com";
+const databasePath = path.join(__dirname, "detailsDatabase.db");
 
-export app.js
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.post("/details/", async (request, response) => {
+  const { email, name } = request.body;
+  const postQuery = `
+  INSERT INTO
+    details (email,name)
+  VALUES
+    (${email}, ${name});`;
+  await database.run(postQuery);
+  response.send("District Successfully Added");
+});
+
+module.exports = app;
