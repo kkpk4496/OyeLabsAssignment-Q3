a23d91602430048c0f593c252e96f6773f77ad4d
kkpk4496 <pavankalyank4496@gmail.com>
2023-06-28 07:21:36 +0000

    main

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
-const express = require("express");
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
-const path = require("path");
+const person = new Object();
+person.id = 2;
+person.gender = "male";
 
-const databasePath = path.join(__dirname, "detailsDatabase.db");
-
-const app = express();
-
-app.use(express.json());
-
-let database = null;
-
-const initializeDbAndServer = async () => {
-  try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
-    });
-
-    app.listen(3000, () =>
-      console.log("Server Running at http://localhost:3000/")
-    );
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
-  }
-};
-
-initializeDbAndServer();
-
-app.post("/", async (request, response) => {
-  const { email, name } = request.body;
-  const postQuery = `
-  INSERT INTO
-    details (email,name)
-  VALUES
-    (${email}, '${name}');`;
-  await database.run(postQuery);
-  response.send("Successfully Added");
-});
-
-module.exports = app;
+const student = new Object();
+student.name = "ravi";
+student.email = "ravi11@yopmail.com";
